d452cc1c7766305162e75718f1d1755b722782a7
Mahdi Hasani <mahdi.hasani@arvancloud.com>
2025-02-13 15:43:55 +0330

    feature: Field component

---
+++ lib/components/Field/Field.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Field } from './Field';
+import { Input } from '../Input/Input.tsx';
+
+const meta = {
+    component: Field,
+} satisfies Meta<typeof Field>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    render({ label, required, message, compact }) {
+        return (
+            <Field
+                label={label}
+                required={required}
+                message={message}
+                compact={compact}
+            >
+                <Input />
+            </Field>
+        );
+    },
+    args: {
+        children: null,
+        label: 'Email Address',
+        required: true,
+        message: 'Enter valid email',
+        compact: true,
+    },
+};
