7c4fd203c1d0341a21cb9b0b7620b5d8dff9283d
Mahdi Hasani <mahdi.hasani@arvancloud.com>
2025-02-13 15:43:55 +0330

    feature: Field component

---
+++ lib/components/Field/Field.tsx
@@ -0,0 +1,58 @@
+import { WarningCircle } from './icons/warning-circle.tsx';
+import {
+    Children,
+    isValidElement,
+    JSXElementConstructor,
+    ReactNode,
+} from 'react';
+import { Input } from '../Input/Input.tsx';
+import { clsx } from 'clsx';
+
+type FieldProps = {
+    label: string;
+    compact?: boolean;
+    required?: boolean;
+    message?: string;
+    children: ReactNode;
+    className: string;
+};
+
+export const Field = ({
+    label,
+    children: _children,
+    required,
+    message,
+    compact,
+    className,
+}: FieldProps) => {
+    const validChildren: (JSXElementConstructor<any> | string)[] = [Input];
+
+    const children = Children.map(_children, (child) => {
+        if (isValidElement(child) && validChildren.includes(child.type)) {
+            return child;
+        }
+        console.warn('Invalid child type:', child);
+        return null;
+    });
+
+    return (
+        <div className={clsx('flex flex-col', className)}>
+            <label
+                htmlFor={'text'}
+                className={`self-start relative bg-neutral-1-default px-0.5 transition ease-in-out ${compact && '-mb-2 ms-2'}`}
+            >
+                {label}
+                {required && (
+                    <span className="ms-1 text-error-1-default">*</span>
+                )}
+            </label>
+            {children}
+            {message && (
+                <div className="flex gap-1 text-caption-c1 text-neutral-5-default mt-1">
+                    <WarningCircle />
+                    {message}
+                </div>
+            )}
+        </div>
+    );
+};
