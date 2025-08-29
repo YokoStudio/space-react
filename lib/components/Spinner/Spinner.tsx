6ef39861a7acf46258537df9f42f28ea89187e4d
Mahdi Hasani <mahdi.hasani@arvancloud.ir>
2025-08-29 21:08:12 +0330

    feature: spinner component

--- lib/components/Spinner/Spinner.tsx
+++ lib/components/Spinner/Spinner.tsx
@@ -1,11 +1,24 @@
 import { clsx } from "clsx";
 import { Icon } from "../Icon/Icon";
+import './style.css'
+import useClassName from "../../utilities/useClassName";
 
-export const Spinner = ({ }: { size?: string }) => {
-    return <div className={clsx('spinner')}>
-        <div className="spinner__icon">
-            <Icon name="circle-loading" />
+export interface SpinnerProp {
+    direction?: 'row' | 'column';
+    size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+    content: string;
+    className?: string;
+}
+
+export const Spinner = ({ direction = 'row', size = 'md', content, className }: SpinnerProp) => {
+    const bemClass = useClassName('spinner');
+
+    return <div className={clsx(bemClass(), bemClass('direction', direction), className)}>
+        <div className={bemClass('icon')}>
+            <Icon name="circle-loading" size={size} className={bemClass('icon', 'svg')} />
         </div>
-        <div className="spinner__content">Loading...</div>
+        <div className={clsx(bemClass('content'), bemClass('content', size))}>
+            {content}
+        </div>
     </div>;
 };
