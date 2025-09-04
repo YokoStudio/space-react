cecd34ebb6c704db48ab97e68ef865c82c0f469d
Mahdi Hasani <mahdi.hasani@arvancloud.ir>
2025-09-04 22:12:54 +0330

    improve: add prepend button and loading and clear button

--- lib/constants/icons-map.ts
+++ lib/constants/icons-map.ts
@@ -1,5 +1,9 @@
 import CircleLoading from '../icons/circle-loading.svg?react';
+import Search from '../icons/search.svg?react';
+import CrossCircle from '../icons/cross-circle.svg?react';
 
 export const iconsMap = {
     'circle-loading': CircleLoading,
+    'search': Search,
+    'cross-circle': CrossCircle,
 };
